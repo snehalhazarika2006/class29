31625db712ce71675f2eaad756ab5ba7cddd1101
snehalhazarika2006 <hazarikasnehal@gmail.com>
2021-10-02 17:46:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+let engine;
+let world;
+var ground;
+var rope;
+var fruit;
+var fruit_con;
+
+function setup() {
+  createCanvas(500,700);
+  frameRate(80);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(200,690,600,20);
+  rope = new Rope(6, {x : 250, y : 40});
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+  textSize(50);
+
+  var fruitOptions = {
+    density : 0.001
+  }
+
+  fruit = Bodies.circle(300, 300, 15, fruitOptions);
+  Matter.Composite.add(rope.body,fruit);
+
+  fruit_con = new Link(rope, fruit);
+  
+}
+
+function draw() {
+  Engine.update(engine);
+  background(51);
+  ground.show();
+  rope.show();
+
+  ellipse(fruit.position.x, fruit.position.y, 15, 15);  
+}
